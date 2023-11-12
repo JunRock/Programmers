874a9afa479b43302e165621367427087e88c1d2
leejunseok <wnstjr120422@naver.com>
2023-11-12 20:39:56 +0900

    feat: add 옹알이 추가

---
+++ src/CodingTestPractice/옹알이.java
@@ -0,0 +1,22 @@
+package CodingTestPractice;
+
+import java.util.Scanner;
+
+public class 옹알이 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String[] str= {"aya", "yee", "u", "maa", "wyeoo"};
+        int count=run(str);
+        System.out.println(count);
+    }
+
+    public static int run(String[] arr){
+        String[] test={"aya", "ye", "woo", "ma"};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<test.length;j++){
+
+            }
+        }
+        return 1;
+    }
+}
